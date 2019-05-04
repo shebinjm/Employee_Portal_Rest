75ed0a8212988910f8c32062b334e55a4ce7c0b2
shebinjm <shebinjm@gmail.com>
2019-05-04 10:07:27 +0530

    Error page handling error fixed

--- src/main/java/com/employee/rest/empmngr/EmpMngrApplication.java
+++ src/main/java/com/employee/rest/empmngr/EmpMngrApplication.java
@@ -1,13 +1,29 @@
 package com.employee.rest.empmngr;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class EmpMngrApplication {
 
+	@Autowired
+    private DispatcherServlet servlet;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EmpMngrApplication.class, args);
 	} 
+	
+	@Bean
+    public CommandLineRunner getCommandLineRunner(ApplicationContext context) {
+        servlet.setThrowExceptionIfNoHandlerFound(true);
+        return args -> {};
+    }
 
 }
